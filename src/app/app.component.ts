3407cb56c4855ca7212d4462693b9447e57a8854
gauravprwl14 <gauravprwl14@gmail.com>
2017-11-03 23:58:33 +0530

    added loader component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  showLoader = true;
+  ngOnInit() {
+    document.addEventListener('DOMContentLoaded', (event) => {
+      setTimeout(() => {
+        this.showLoader = false;
+      }, 2000);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule
