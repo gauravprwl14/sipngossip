5bf2018fb0e9c89a23e56ea6972f91a3688f04ff
gauravprwl14 <gauravprwl14@gmail.com>
2017-11-24 02:34:44 +0530

    added images for drinks menu

--- src/app/shared/fakeData/menuItem/drinks.ts
+++ src/app/shared/fakeData/menuItem/drinks.ts
@@ -1,6 +1,6 @@
 const drinksMenuItemArr = [
   {
-    imgSrcPath: "assets/images/menu/img1.png",
+    imgSrcPath: "assets/images/menu/drinks/lemonIcedTea.png",
     name: "Lemon Ice Tea",
     description: "food description should be brief and to the point",
     foodTypeVege: true,
@@ -9,7 +9,7 @@ const drinksMenuItemArr = [
     subCategoryName: ""
   },
   {
-    imgSrcPath: "assets/images/menu/img1.png",
+    imgSrcPath: "assets/images/menu/drinks/rosePetal.png",
     name: "Rose Petal Desire",
     description: "food description should be brief and to the point",
     foodTypeVege: true,
@@ -18,7 +18,7 @@ const drinksMenuItemArr = [
     subCategoryName: ""
   },
   {
-    imgSrcPath: "assets/images/menu/img1.png",
+    imgSrcPath: "assets/images/menu/drinks/masalaShikanji.png",
     name: "Masala Shikanji",
     description: "food description should be brief and to the point",
     foodTypeVege: true,
@@ -27,7 +27,7 @@ const drinksMenuItemArr = [
     subCategoryName: ""
   },
   {
-    imgSrcPath: "assets/images/menu/img1.png",
+    imgSrcPath: "assets/images/menu/drinks/coldDrink.png",
     name: "Cold Drinks",
     description: "food description should be brief and to the point",
     foodTypeVege: true,
@@ -36,7 +36,7 @@ const drinksMenuItemArr = [
     subCategoryName: ""
   },
   {
-    imgSrcPath: "assets/images/menu/img1.png",
+    imgSrcPath: "assets/images/menu/drinks/jalJeera.png",
     name: "Jal Jeera",
     description: "food description should be brief and to the point",
     foodTypeVege: true,
