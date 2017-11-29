5a534d652d5ca4662a71b3530a0c37052c066771
gauravprwl14 <gauravprwl14@gmail.com>
2017-11-29 15:49:01 +0530

    added images for raita

--- src/app/shared/fakeData/menuItem/raita.ts
+++ src/app/shared/fakeData/menuItem/raita.ts
@@ -1,6 +1,6 @@
 const raitaMenuItemArr = [
   {
-    imgSrcPath: "assets/images/menu/img1.png",
+    imgSrcPath: "assets/images/menu/raita/vegRaita.png",
     name: "Veg. Raita",
     description: "food description should be brief and to the point",
     foodTypeVege: true,
@@ -9,7 +9,7 @@ const raitaMenuItemArr = [
     subCategoryName: ""
   },
   {
-    imgSrcPath: "assets/images/menu/img1.png",
+    imgSrcPath: "assets/images/menu/raita/bundiRaita.png",
     name: "Bundi Raita",
     description: "food description should be brief and to the point",
     foodTypeVege: true,
@@ -18,7 +18,7 @@ const raitaMenuItemArr = [
     subCategoryName: ""
   },
   {
-    imgSrcPath: "assets/images/menu/img1.png",
+    imgSrcPath: "assets/images/menu/raita/pineappleRaita.png",
     name: "Pineapple Raita",
     description: "food description should be brief and to the point",
     foodTypeVege: true,
@@ -27,7 +27,7 @@ const raitaMenuItemArr = [
     subCategoryName: ""
   },
   {
-    imgSrcPath: "assets/images/menu/img1.png",
+    imgSrcPath: "assets/images/menu/raita/onionPudinaRaita.png",
     name: "Onion Pudina Raita",
     description: "food description should be brief and to the point",
     foodTypeVege: true,
