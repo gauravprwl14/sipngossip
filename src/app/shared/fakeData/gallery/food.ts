79209e89d1dcef9ce63459822ca74d1f5f9aec61
gauravprwl14 <gauravprwl14@gmail.com>
2017-11-29 07:29:52 +0530

    added food related gallery items

--- src/app/shared/fakeData/gallery/food.ts
+++ src/app/shared/fakeData/gallery/food.ts
@@ -26,6 +26,97 @@ const foodItemsArr = [
     description: "food description should be brief and to the point",
     foodTypeVege: true,
     category: "food"
+  },
+  {
+    imgSrcPath: "assets/images/food/aluParatha/aluParatha1.png",
+    name: "Alu Paratha",
+    description: "food description should be brief and to the point",
+    foodTypeVege: true,
+    category: "food"
+  },
+  {
+    imgSrcPath: "assets/images/food/aluParatha/aluParatha2.png",
+    name: "Alu Paratha",
+    description: "food description should be brief and to the point",
+    foodTypeVege: true,
+    category: "food"
+  },
+  //   {
+  //     imgSrcPath: "assets/images/food/aluParatha/aluParatha3.png",
+  //     name: "Alu Paratha",
+  //     description: "food description should be brief and to the point",
+  //     foodTypeVege: true,
+  //     category: "food"
+  //   },
+  {
+    imgSrcPath: "assets/images/food/oreoShake/oreoShake1.png",
+    name: "Oreo Shake",
+    description: "food description should be brief and to the point",
+    foodTypeVege: true,
+    category: "food"
+  },
+  {
+    imgSrcPath: "assets/images/food/oreoShake/oreoShake2.png",
+    name: "Oreo Shake",
+    description: "food description should be brief and to the point",
+    foodTypeVege: true,
+    category: "food"
+  },
+  {
+    imgSrcPath: "assets/images/food/cigarRoll/cigarRoll1.png",
+    name: "Cigar Role",
+    description: "food description should be brief and to the point",
+    foodTypeVege: true,
+    category: "food"
+  },
+  {
+    imgSrcPath: "assets/images/food/cigarRoll/cigarRoll2.png",
+    name: "Cigar Role",
+    description: "food description should be brief and to the point",
+    foodTypeVege: true,
+    category: "food"
+  },
+  {
+    imgSrcPath: "assets/images/food/cigarRoll/cigarRoll3.png",
+    name: "Cigar Role",
+    description: "food description should be brief and to the point",
+    foodTypeVege: true,
+    category: "food"
+  },
+  {
+    imgSrcPath: "assets/images/food/lemonTea/lemonTea1.png",
+    name: "Lemon Tea",
+    description: "food description should be brief and to the point",
+    foodTypeVege: true,
+    category: "food"
+  },
+  {
+    imgSrcPath: "assets/images/food/lemonTea/lemonTea2.png",
+    name: "Lemon Tea",
+    description: "food description should be brief and to the point",
+    foodTypeVege: true,
+    category: "food"
+  },
+  {
+    imgSrcPath: "assets/images/food/lemonTea/lemonTea3.png",
+    name: "Lemon Tea",
+    description: "food description should be brief and to the point",
+    foodTypeVege: true,
+    category: "food"
+  },
+  {
+    imgSrcPath: "assets/images/food/panShot/panShot1.png",
+    name: "Pan Shot",
+    description: "food description should be brief and to the point",
+    foodTypeVege: true,
+    category: "food"
+  },
+  {
+    imgSrcPath: "assets/images/food/panShot/panShot2.png",
+    name: "Pan Shot",
+    description: "food description should be brief and to the point",
+    foodTypeVege: true,
+    category: "food"
   }
 ];
 
