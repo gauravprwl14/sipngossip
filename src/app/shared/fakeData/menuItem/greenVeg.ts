20435df936207bcb749629a0a0129c514ce90977
gauravprwl14 <gauravprwl14@gmail.com>
2017-11-29 15:34:57 +0530

    added images for veg green

--- src/app/shared/fakeData/menuItem/greenVeg.ts
+++ src/app/shared/fakeData/menuItem/greenVeg.ts
@@ -1,7 +1,7 @@
 const greenVegMenuItemArr = [
   {
-    imgSrcPath: "assets/images/menu/img1.png",
-    name: "Veg. Khada Masala",
+    imgSrcPath: "assets/images/menu/greenVeg/vegKadaiPaneer.png",
+    name: "Veg. Kadai Masala",
     description: "food description should be brief and to the point",
     foodTypeVege: true,
     category: "greenVeg",
@@ -9,7 +9,7 @@ const greenVegMenuItemArr = [
     subCategoryName: ""
   },
   {
-    imgSrcPath: "assets/images/menu/img1.png",
+    imgSrcPath: "assets/images/menu/greenVeg/tawaVegetable.png",
     name: "Tawa Vegetable",
     description: "food description should be brief and to the point",
     foodTypeVege: true,
@@ -18,7 +18,7 @@ const greenVegMenuItemArr = [
     subCategoryName: ""
   },
   {
-    imgSrcPath: "assets/images/menu/img1.png",
+    imgSrcPath: "assets/images/menu/greenVeg/vegJalfrezi.png",
     name: "Veg. Jalfrezi",
     description: "food description should be brief and to the point",
     foodTypeVege: true,
@@ -27,16 +27,7 @@ const greenVegMenuItemArr = [
     subCategoryName: ""
   },
   {
-    imgSrcPath: "assets/images/menu/img1.png",
-    name: "Veg. Jalfrezi",
-    description: "food description should be brief and to the point",
-    foodTypeVege: true,
-    category: "greenVeg",
-    subCategoryPresent: false,
-    subCategoryName: ""
-  },
-  {
-    imgSrcPath: "assets/images/menu/img1.png",
+    imgSrcPath: "assets/images/menu/greenVeg/dumSumHandi.png",
     name: "Dum Sum Handi",
     description: "food description should be brief and to the point",
     foodTypeVege: true,
